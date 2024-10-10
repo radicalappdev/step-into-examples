ac7d55f5fe36be2619f691144044f7f5e727de5c
Joseph Simpson <joe@radicalappdev.com>
2024-10-10 05:18:54 -0400

    Starting Garden 10

---
+++ Garden10/Garden10/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Garden10
+//
+//  Created by Joseph Simpson on 10/10/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Garden10/Garden10/Garden10App.swift
@@ -0,0 +1,34 @@
+//
+//  Garden10App.swift
+//  Garden10
+//
+//  Created by Joseph Simpson on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Garden10App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
