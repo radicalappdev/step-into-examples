afe4c1a12c0894d5d38731c64a5cd0f47cffd7cd
Joseph Simpson <joe@radicalappdev.com>
2024-11-19 15:25:30 -0500

    Clones!

--- Garden12/Garden12/ImmersiveView.swift
+++ Garden12/Garden12/ImmersiveView.swift
@@ -10,29 +10,41 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
-
     var body: some View {
         RealityView { content, attachments in
+            if let root = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(root)
 
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
-
-                if let glassSphere = immersiveContentEntity.findEntity(named: "GlassSphere") {
-
-                let clone = glassSphere.clone(recursive: true)
-                    content.add(clone)
-                    clone.position = .init(x: 0.7, y: 1.5, z: -1.5)
+                if let glassSphere = root.findEntity(named: "GlassSphere") {
+                    createClones(root, glassSphere: glassSphere)
                 }
-
-
             }
         } update: { content, attachments in
         } attachments: {
+
+        }
+    }
+
+    func createClones(_ root: Entity, glassSphere: Entity) {
+        let centerPos = SIMD3<Float>(0, 1.5, 0)
+        for _ in 1...100 {
+            let clone = glassSphere.clone(recursive: true)
+            // Random spherical coordinates - thanks ChatGPT!
+            let distance = Float.random(in: 1...3) // Radius between 1 and 3
+            let theta = Float.random(in: 0...(2 * .pi)) // Random angle for the horizontal plane
+            let phi = Float.random(in: 0...(Float.pi)) // Random angle for the vertical plane
+
+            // Convert spherical coordinates to Cartesian
+            let x = distance * sin(phi) * cos(theta)
+            let y = distance * sin(phi) * sin(theta)
+            let z = distance * cos(phi)
+            clone.position = centerPos + SIMD3(x, y, z)
+            root.addChild(clone)
         }
     }
 }
-
-#Preview(immersionStyle: .mixed) {
-    ImmersiveView()
-        .environment(AppModel())
-}
+//
+//#Preview(immersionStyle: .mixed) {
+//    ImmersiveView()
+//        .environment(AppModel())
+//}
