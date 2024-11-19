1fcfc13eee2ea148d4d94ea4934f72e04733c3d3
Joseph Simpson <joe@radicalappdev.com>
2024-11-19 15:01:33 -0500

    Actually adding the project...

---
+++ Garden12/Garden12/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Garden12
+//
+//  Created by Joseph Simpson on 11/19/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ Garden12/Garden12/Garden12App.swift
@@ -0,0 +1,33 @@
+//
+//  Garden12App.swift
+//  Garden12
+//
+//  Created by Joseph Simpson on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Garden12App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
